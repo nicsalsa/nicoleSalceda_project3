7e0c2da4e2ad1df1527f2ce1254619c27ae7fd6c
Nicole <ensalced@gmail.com>
2018-08-16 15:18:07 -0400

    created object and array to display results

--- scripts/script.js
+++ scripts/script.js
@@ -1,103 +1,135 @@
 
 // make objects for relationship, mode, tone, and emphasis
 
+// for each object, make properties that produce a value. write a title for each value produced.
+// 0 = dontUse
+// 10 = useWithCaution
+// 20 = use
+
 const quizApp = {};
 let total = 0;
 
-quizApp.submitChoices = function () {
-      $('form').on('submit', function (e) {
+quizApp.Results = {
+   answer1: { 
+      professional: [{
+            message: "Professional? Best practice is not to bang in the workplace.",
+            tally: 0,
+      }],
+      personal: [{
+            message: "Personal? Bang it up!",
+            tally: 20,
+      }],
+      audience: [{
+            message: "Hmm, think about this a little more.",
+            tally: 10,
+      }],
+   },
+   answer2: {
+      email: [{
+            message: "Hmm, think about this a little more.",
+            tally: 10,
+      }],
+      messenger: [{
+            message: "Do it up!!!",
+            tally: 20,
+      }],
+      post: [{
+            message: "hmm, think about this a little more.",
+            tally: 10,
+      }],
+      letter: [{
+            message: "Hmm, think about this a little more.",
+            tally: 10,
+      }],
+   },
+   answer3: {
+      joyful: [{
+            message: "If you're joyful and you know it use a bang!",
+            tally: 20,
+      }],
+      humorous: [{
+            message: "Hmm, Think a little more about it",
+            tally: 10,
+      }],
+      informal: [{
+            message: "Why not?!",
+            tally: 20,
+      }],
+      optimistic: [{
+            message: "You can do it!",
+            tally: 20,
+      }],
+      serious: [{
+            message: "Unless you are serious about banging, don't do it.",
+            tally: 0,
+      }],
+      formal: [{
+            message: "Definitely, don't do it",
+            tally: 0,
+      }],
+      sad: [{
+            message: "No one likes a cry bang. Don't be weird.",
+            tally: 0
+      }],
+      pessimistic: [{
+            message: "Unless you change your attitude, don't expect to bang.",
+            tally: 0
+      }],
+      horror: [{
+            message: "There's a time and a place to bang, this isn't it.",
+            tally: 0
+      }],
+   },
+   answer4: {
+         astonishment: [{
+               message: "I'm amazed! I'm astonished!",
+               tally: 20,
+            }],
+         shock: [{
+               message: "I'm so surprised and have mixed feelings!",
+               tally: 20,
+            }], 
+         excitement: [{
+               message: "I'm eager!",
+               tally: 20,
+            }],
+         urgency: [{
+               message: "I need swift action to be taken!",
+               tally: 20,
+            }],
+         vehemence: [{
+               message: "I am intensely passionate about this!",
+               tally: 20,
+            }],
+         nodeclare: [{
+               message: "I'm pretty chill right now",
+               tally: 0,
+            }],
+   }
+}
+
+
+quizApp.submitChoices = function(){
+      $('form').on('submit', function(e) {
             e.preventDefault();
-            const relationAnswer = $('input[name = relation]:checked').val()
-            console.log(relationAnswer);
-            const modeAnswer = $('input[name = mode]:checked').val()
-            // console.log(modeAnswer);
-            const toneAnswer = $('input[name = tone]:checked').val()
-            // console.log(toneAnswer);
+            const relationAnswer = $('input[name = relation]:checked').val();
+            const modeAnswer = $('input[name = mode]:checked').val();
+            const toneAnswer = $('input[name = tone]:checked').val();
             const emphasisAnswer = $('input[name = emphasis]:checked').val();
-            // console.log(emphasisAnswer);
             const answerArray = [];
             answerArray.push(relationAnswer, modeAnswer, toneAnswer, emphasisAnswer);
             // console.log(answerArray);
 
             answerArray.forEach((item) => {
                   total += parseInt(item);
-                  console.log(item);
+                  // console.log(item);
+                  console.log(total);
             })
-            console.log(total);
+            
       });
 }
 
+quizApp.submitChoices();
 
 
-
-// for each object, make properties that produce a value. write a title for each value produced.
-// 0 = dontUse
-// 10 = useWithCaution
-// 20 = use
-
-
-// quizApp.answers = {
-//    answer1: { 
-//       professional: 0,
-//       personal: 20,
-//       audience: 10
-//    },
-//    answer2: {
-//       email: 10,
-//       messenger: 20,
-//       post: 10,
-//       letter: 0
-//    },
-//    answer3: {
-//       joyful: 20,
-//       humorous: 20,
-//       informal: 20,
-//       optimistic: 20,
-//       serious: 0,
-//       formal: 0,
-//       sad: 0,
-//       pessimistic: 0,
-//       horror: 0
-//    },
-//    answer4: {
-//       astonishment: [
-//          "I'm amazed! I'm astonished!",
-//          20,
-//       ],
-//       shock: [
-//          "I'm so surprised and have mixed feelings!", 
-//          20,
-//       ],
-//       excitement: [
-//          "I'm eager!", 
-//          20,
-//       ],
-//       urgency: [
-//          "I need swift action to be taken!",
-//          20,
-//       ],
-//       vehemence: [
-//          "I am intensely passionate about this!",
-//          20,
-//       ],
-//       nodeclare: [
-//          "I'm pretty chill right now", 
-//          0,
-//       ],
-//    }
-// }
-
-
-// based on what the user selects, they will either accumulate points on each question or not.
-
-// {} objects
-// () functions
-// [] arrays
-
-
-
-
-
-// the more points they have, the more acceptable it is to use exclamation points. 
-
-// when they submit the quiz, it displays each option they selected.
+// when the user checks a radio button, 
