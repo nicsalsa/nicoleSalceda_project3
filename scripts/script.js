4657487666b016f5456f2798ce734159115cd06e
Nicole <ensalced@gmail.com>
2018-08-20 09:05:25 -0400

    Final Commit

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ $(function(){
         quizApp.submitChoices();
 });
 // objects and arrays
+
 quizApp.results = {
    answer1: { 
       professional: [{
@@ -126,32 +127,37 @@ quizApp.submitChoices = function(){
             // when input is selected, append the variable that holds the message in the array
             const selectedId1 = $('.relationship input[type = radio]:checked').attr('id');
             const results1 = quizApp.results.answer1[selectedId1][0].message;
-            $('.relationship-answer').append(results1);
+            // $('.relationship-answer').append(results1);
 
             const selectedId2 = $('.mode input[type = radio]:checked').attr('id');
             const results2 = quizApp.results.answer2[selectedId2][0].message;
-            $('.mode-answer').append(results2);
+            // $('.mode-answer').append(results2);
 
             const selectedId3 = $('.tone input[type = radio]:checked').attr('id');
             const results3 = quizApp.results.answer3[selectedId3][0].message;
-            $('.tone-answer').append(results3);
+            // $('.tone-answer').append(results3);
 
             const selectedId4 = $('.emphasis input[type = radio]:checked').attr('id');
             const results4 = quizApp.results.answer4[selectedId4][0].message;
-            $('.emphasis-answer').append(results4);
+            // $('.emphasis-answer').append(results4);
             // if else function to populate results
             if(total < 50){
                   let dontUseDisplay = $('h2.answers').text(
                         'No bang necessary!'
-                  );
+                  ).append(
+                        `<p class="subtext">Many of you eager bangers may have been dissappointed to find that the bang is frowned upon in professional, sad, and/or seriously horrific situations. I believe in you, though. There's a whole brain full of words right in your head that can help you find other ways to convey that emotion.</p>
+                        <div class="image"><img src="https://d24bnpykhxwj9p.cloudfront.net/s3file/s3fs-public/users1/2016-10/Mon/6359271129309535251975219305_giphy.gif" alt="trump being the best at words"></div>`);
             } else if(total >= 50 && total <= 60){
                   let cautionDisplay = $('h2.answers').text(
                         'Use with caution'
-                  );
+                  ).append(`<p class="subtext"> In this day and age we rely heavily on exclamation points for more than just conveying joy, but also to set a tone of politeness. Be cautious with this choice! You may be able to express yourself better with words.</p>
+                  <img class="image" src="https://data.whicdn.com/images/29153382/original.gif" alt="Family guy watching a stick man slip on a wet floor with wet floor sign.">`);
             } else {
                   let useDisplay = $('h2.answers').text(
-                        'Bang at will!!!'
-                  );
+                        'Bang at will !!!'
+                  ).append(
+                        `<p class="subtext"> It is totally appropriate to end your thought with a bang! Express that emotion like you're about to get married on top of a mountain, and there's going to be flutes playing and trombones and flowers and garlands of fresh herbs. Then your children will form a family band and tour the countryside because that is what punctuation does.</p>
+                        <div class="image"><img class="burgundy" src="http://www.sharegif.com/wp-content/uploads/2014/05/6-anchorman-quotes-burgundy.gif" alt="I am Ron Burgundy?"></div> `);
             }; 
       });
 }
